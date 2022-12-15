7b0f1dd450ede5aace45f978ece724d51433be34
frank <frnkmendoza101@gmail.com>
2022-12-15 16:47:56 +0800

    add revs

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,29 +2,49 @@ import React from 'react'
 import { Link } from 'react-router-dom';
 
 import { MdClose } from 'react-icons/md';
+import { GrProjects, GrMailOption, GrHomeRounded, GrCatalog } from 'react-icons/gr';
 import Button from '../customComponents/Button'
 
 import navlogo from '../img/logo.svg'
 
-const Sidebar = ({ scrollToElement,openToggle,openSidebar, ...props }) => {
+const Sidebar = ({ scrollToElement, openToggle, openSidebar, ...props }) => {
   return (
-    <div className='sidebar' style={{right: props.toggle ? '-100%' : 0 }}>
-      <Link className='sidebar__logo'
-         to={'/portfolio' } 
-         onClick={() => openSidebar()}
-      >
-        <img src={navlogo} alt="logo" />
-      </Link>
-      <div className="sidebar__links">
-        <ul className="sidebar__ul">
-          <li className="sidebar__links" onClick={() => scrollToElement(true,)}>Home</li>
-          <li className="sidebar__links" onClick={() => scrollToElement(false, props.works)}>Works</li>
-          <li className="sidebar__links" onClick={() => scrollToElement(false, props.about)}>About</li>
-          {/* <li className="sidebar__links" onClick={() => scrollToElement(false, props.contact)}>Contact</li> */}
-        </ul>
+    <div
+      className='sidebar'
+      style={
+        props.toggle
+          ?
+          { right: '-100%', width: 0 }
+          :
+          { right: 0, width: '100%' }}
+      onClick={() => openToggle()}
+    >
+      <div className="sidebar__wrapper">
+        <Link className='sidebar__logo'
+          to={'/portfolio'}
+          onClick={() => openSidebar()}
+        >
+          <img src={navlogo} alt="logo" />
+        </Link>
+        <div className="sidebar__links">
+          <ul className="sidebar__ul">
+            <li className="sidebar__links" onClick={() => scrollToElement(true,)}>
+              <GrHomeRounded size={20} color='#fff' />
+            </li>
+            <li className="sidebar__links" onClick={() => scrollToElement(false, props.works)}>
+              <GrProjects size={20} color='#fff' />
+            </li>
+            <li className="sidebar__links" onClick={() => scrollToElement(false, props.about)}>
+              <GrCatalog size={20} color='#fff' />
+            </li>
+            <li className="sidebar__links" onClick={() => scrollToElement(false, props.contact)}>
+              <GrMailOption size={20} color='#fff' />
+            </li>
+          </ul>
+        </div>
+        {/* <Button name='Send Message' secondary={true} onClick={() => scrollToElement(false, props.contact)}/> */}
+        {/* <MdClose  size={20} style={{position: 'absolute', right: 20, top: 20 }}/> */}
       </div>
-      <Button name='Send Message' secondary={true} onClick={() => scrollToElement(false, props.contact)}/>
-      <MdClose onClick={() => openToggle()} size={20} style={{position: 'absolute', right: 20, top: 20 }}/>
     </div >
   )
 }
