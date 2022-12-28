7b3f3603e7b61ad94b15201e7d3a8f5b7742f20c
Frank Mendoza <frnkmendoza101@gmail.com>
2022-12-28 21:16:42 +0800

    add pdf dowload button

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,34 @@
 import React from "react";
 
 import hero from "../img/hero.png";
-import Button from "../customComponents/Button";
+import pdf from "../pdf/Frank-Mendoza-CV.pdf"
 
 const Home = ({ location, dark }) => {
+  const onButtonClick = () => {};
+
   return (
-    <div className={!dark ? "home" : 'home dark'} id="home" ref={location}>
+    <div className={!dark ? "home" : "home dark"} id="home" ref={location}>
       <div className="home__container">
         <div className="home__hero">
-          <img src={hero} alt=""/>
+          <img src={hero} alt="" />
         </div>
-        <div className={!dark ? "home__contents" : 'home__contents dark'}>
+        <div className={!dark ? "home__contents" : "home__contents dark"}>
           <h1 className="home__title ">
             Hi! <br />
             I'm Frank
           </h1>
           <h3 className="home__subtitle">Frontend Developer</h3>
-          {/* <Button dark={dark} black={true} name="Donwload CV"/> */}
+
+          <a
+            href={pdf}
+            target="_blank"
+            className={!dark ? "button black" : "button dark"}
+            style={{ width: "150px" }}
+            onClick={() => onButtonClick()}
+            download
+          >
+            Donwload CV
+          </a>
         </div>
       </div>
     </div>
